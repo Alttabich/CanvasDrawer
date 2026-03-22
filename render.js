2e56ba50ee41beb47ad5b715cc9e71d491d63544
Alttabich <dmitrayxd@gmail.com>
2026-03-23 01:11:37 +0300

    bouncing cube

--- render.js
+++ render.js
@@ -19,18 +19,40 @@ const CUBE = [
     { x: -0.5, y: 0.5, z: -0.5 },
 ]
 
-function translate({x, y, z}, {dx, dy, dz})
+var grid = new Array();
+var connections = new Array
+function createGrid(gridArray, N)
+{	
+	for (let i = 0; i < N; i++)
+	{
+		for (let j = 0; j < N; j++)
+		{
+			Point = {};
+			Point.x = (i / N * 2) - 1;
+			Point.y = -0.25;
+			Point.z = j * 5 / N;
+			//0 -> N
+			//0 -> 1
+			//0 -> 2
+			//-1 -> 1
+			//console.log(Point)	
+			gridArray.push(Point);
+		}
+	}
+}
+
+function translate({x, y, z}, translation)
 {
     return {
-        x: x + dx,
-        y: y + dy,
-        z: z + dz,
+        x: x + translation.x,
+        y: y + translation.y,
+        z: z + translation.z,
     };
 }
 
-function rotate({x, y, z}, angle = 0)
+function rotate({x, y, z}, angle = 0, yRotation = false)
 {
-    return {
+	return {
         x: x * Math.cos(angle) - z * Math.sin(angle),
         y,
         z: x * Math.sin(angle) + z * Math.cos(angle)
@@ -46,9 +68,7 @@ function toScreenSpace(p)
 
 function projectOnScreen(p)
 {
-    p.x = p.x / p.z;
-    p.y = p.y / p.z;
-    return p;
+    return {x: p.x / p.z, y: p.y / p.z, z: p.z};
 }
 
 function drawStars(Point, BlinkPeriod) //@TODO: make stars flick more naturally
@@ -73,6 +93,7 @@ function drawLine(a, b, color="green")
     ctx.beginPath();
     ctx.moveTo(a.x, a.y);
     ctx.strokeStyle = color;
+	ctx.lineWidth = 5;
     ctx.lineTo(b.x, b.y);
     ctx.stroke();
 }
@@ -118,14 +139,38 @@ function CreateRandomPointsArray(ArrayHandle, numOfPointsX, numOfPointsY)
     }
 }
 
-
-
+function calculateTranslation(vector, velocity, dt)
+{
+	if (vector.z > 5 )
+		velocity.z = -VZ;
+	else
+		if(vector.z <=1 )
+			velocity.z = VZ;
+	if (vector.y > -0.25)
+		velocity.y -= dt * g;
+	else 
+	{
+		velocity.y += dt * g;
+		
+	}
+	vector.x = vector.x + velocity.x * dt;
+	vector.y = vector.y + velocity.y * dt;
+	vector.z = vector.z + velocity.z * dt;
+}
+var g = 9.8
 var t = 0;
 var angle = 0;
+var VZ = 1;
+var velocity = {x: 0, y: 1, z: VZ}
+var translation = {x:0, y:0, z:0};
+
+createGrid(grid, 100);
+
 function mainLoop()
 {
-    const translation = {dx: 0, dy: 0, dz: t}
-    dt = 1 / FPS;
+    const dt = 1 / FPS;
+	calculateTranslation(translation, velocity, dt);
+	//console.log(translation)
     angle = t;
     clearScreen();
     for (const Point of starsArray)
@@ -133,12 +178,19 @@ function mainLoop()
         drawStars(Point, 2 * Math.sin(t));        
     }
     var projectedVertices = new Array();
+    var projectedGridVertices = new Array();
     for (const Vertex of CUBE)
     {
         let ProjectedVertex = toScreenSpace(projectOnScreen(translate(rotate(Vertex, angle), translation)));
         drawPixelOnScreen(ProjectedVertex, "red", 5);
         projectedVertices.push(ProjectedVertex);
     }
+	for (const Vertex of grid)
+	{
+		let ProjectedVertex = toScreenSpace(projectOnScreen((Vertex)));
+        drawPixelOnScreen(ProjectedVertex, "purple", 10 / (1 + Vertex.z));
+        projectedGridVertices.push(ProjectedVertex);
+	}
     for (i = 0; i < projectedVertices.length / 2; i++)
     {
         drawLine(
@@ -154,10 +206,11 @@ function mainLoop()
                     projectedVertices[i + projectedVertices.length / 2]
                 );
     }
+
     t += dt;
     setTimeout(mainLoop, 1000/FPS); 
 }
 
-CreateRandomPointsArray(starsArray, 20, 20);
+CreateRandomPointsArray(starsArray, 100, 100);
 
 setTimeout(mainLoop, 1000/FPS);
